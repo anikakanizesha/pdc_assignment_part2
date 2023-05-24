1dfa0e2058c9d6ee2206b344dbd4c9f65483973f
anikakanizesha <105477189+anikakanizesha@users.noreply.github.com>
2023-05-24 17:05:28 +1200

    This is the programming assignment

---
+++ src/pdc_assignment_part2/PDC_assignment_part2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pdc_assignment_part2;
+
+/**
+ *
+ * @author anika
+ */
+public class PDC_assignment_part2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
